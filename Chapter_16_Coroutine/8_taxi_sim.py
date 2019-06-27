ea083aadf5c37e724e549e748fe45eb6425acf28
Steven HU <SESA507589@se.com>
2019-06-27 10:49:04 +0800

    0627_2

--- Chapter_16_Coroutine/8_taxi_sim.py
+++ Chapter_16_Coroutine/8_taxi_sim.py
@@ -14,15 +14,18 @@ DEPARTURE_INTERVAL = 5
 Event = collections.namedtuple('Event', 'time proc action')
 
 # BEGIN TAXI_PROCESS
-def taxi_process(ident, trips, start_time=0):
+def taxi_process(ident, trips, start_time=0): 
     """Yield to simulator issuing event at each state change"""
     time = yield Event(start_time, ident, 'leave garage')
+
     for i in range(trips):
         time = yield Event(time, ident, 'pick up passenger')
         time = yield Event(time, ident, 'drop off passenger')
 
+    # 最后一个Yield中没有赋值给time是因为这个time没有意义了，该
+    # process已经结束，后续simulator不会再有这个Process的time了
     yield Event(time, ident, 'going home')
-    # end of taxi process
+    # end of taxi process ---># !!!When the coroutine falls off the end, the generator object raises StopIteration.
 # END TAXI_PROCESS
 
 
@@ -30,7 +33,11 @@ def taxi_process(ident, trips, start_time=0):
 class Simulator:
 
     def __init__(self, procs_map):
+        # PriorityQueue是一个根据item[0](这里是time值)的大小来自动排序的队列
         self.events = queue.PriorityQueue()
+
+        # 这里根据procs_map重新build一个dict，相当于做了copy，因为在运行中self.procs
+        # 的值会改变（删除），而作者不想改变用户传递进来的值，即Main中的taxis
         self.procs = dict(procs_map)
 
     def run(self, end_time):
@@ -88,3 +95,42 @@ def main(end_time=DEFAULT_END_TIME, num_taxis=DEFAULT_NUMBER_OF_TAXIS,
     sim = Simulator(taxis)
     sim.run(end_time)
 
+
+if __name__ == "__main__":
+    taxi = taxi_process(ident=13, trips=2, start_time=0)
+    print(next(taxi))
+    print(taxi.send(7))
+    print(taxi.send(30))
+    print(taxi.send(35))
+    print(taxi.send(83))
+    print(taxi.send(84))
+    print(taxi.send(94)) # 上一个已经触发了going home, 这个send直接触发raise stopiteration
+    '''
+    Output
+        Event(time=0, proc=13, action='leave garage')
+        Event(time=7, proc=13, action='pick up passenger')
+        Event(time=30, proc=13, action='drop off passenger')
+        Event(time=35, proc=13, action='pick up passenger')
+        Event(time=83, proc=13, action='drop off passenger')
+        Event(time=84, proc=13, action='going home')
+        Traceback (most recent call last):
+        File "<stdin>", line 1, in <module>
+        StopIteration
+    '''
+
+    '''
+    parser = argparse.ArgumentParser(
+                        description='Taxi fleet simulator.')
+    parser.add_argument('-e', '--end-time', type=int,
+                        default=DEFAULT_END_TIME,
+                        help='simulation end time; default = %s'
+                        % DEFAULT_END_TIME)
+    parser.add_argument('-t', '--taxis', type=int,
+                        default=DEFAULT_NUMBER_OF_TAXIS,
+                        help='number of taxis running; default = %s'
+                        % DEFAULT_NUMBER_OF_TAXIS)
+    parser.add_argument('-s', '--send', type=int, default=None,
+                        help='random generator seed (for testing)')
+    args = parser.parse_args()
+    main(args.end_time, args.taxis, args.send)
+    '''
